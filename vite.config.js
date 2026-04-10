e072c770d3bf9756cf8bf8e541e90293880f61e5
dronybits <120889687+dronybits@users.noreply.github.com>
2026-04-10 17:00:22 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Ensures assets are linked relatively, which is safer for Vercel deployments
-  base: './',
 })
